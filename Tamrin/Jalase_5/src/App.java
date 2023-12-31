99775a81673b7c76bf6ed15c1bc170f214b58412
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2023-12-31 11:33:30 +0330

    Correcting .gitignore.

--- Tamrin/Jalase_5/src/App.java
+++ Tamrin/Jalase_5/src/App.java
@@ -2,6 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         Book book = new Book("ShahName", "Ferdosi");
         System.out.println(book);
+
+        Student student = new Student(12, 12);
+        System.out.println(student.getGrade());
+        student.setGrade(11);
+        System.out.println(student.getGrade());
     }
 }
 
@@ -17,5 +22,29 @@ class Book {
     @Override
     public String toString() {
         return "Book " + this.title + " by " + this.author;
+    }
+}
+
+class Student {
+    private int studentId;
+    private int grade;
+
+    public Student(int studentId, int grade) {
+        this.studentId = studentId;
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
 }
