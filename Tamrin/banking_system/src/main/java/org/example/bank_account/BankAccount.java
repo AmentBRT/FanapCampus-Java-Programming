41be98686f40c5797ec3bc5043dc3305f9a891aa
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-01-01 16:36:35 +0330

    Initial commit, with BankAccount class.

---
+++ Tamrin/banking_system/src/main/java/org/example/bank_account/BankAccount.java
@@ -0,0 +1,27 @@
+package org.example.bank_account;
+
+public class BankAccount {
+    private int accountNumber;
+    private String ownerName;
+    private int balance;
+
+    BankAccount(int accountNumber, String ownerName, int balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
